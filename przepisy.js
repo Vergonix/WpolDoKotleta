0dcbdab484bf8c6bb374020df3de1212f79e7ccd
Paweł Pietralik <pietralikpawel@gmail.com>
2017-05-18 16:36:32 +0200

    Adding ng-route into project files

--- przepisy.js
+++ przepisy.js
@@ -1,30 +1,34 @@
 var url = 'https://api.trello.com/1/search?idBoards=54ad9476179886c979b60080';
 var apiKey = '4cc334d87ada29b1884404fe3f30956a';
 
-var xhttp = new XMLHttpRequest();
-xhttp.open('GET', url, true);
+var app = angular.module("myApp", ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when("/main", {
+            templateUrl: "main.html",
+            controller: "myCtrl"
+        })
+        .when("/zaloguj", {
+            templateUrl: "zaloguj.html"
+        })
+        .when("/zarejestruj", {
+            templateUrl: "zarejestruj.html"
+        })
+        .when("/ulubione", {
+            templateUrl: "ulubione.html"
+        })
+        .when("/menuUzytkownika", {
+            templateUrl: "menuUzytkownika.html"
+        })
+        .otherwise({
+            redirectTo: $routeProvider});
+}]);
 
-$('#btn').click(function() {
-    $("#recipe").html('');
-    
-    var cokolwiek = $("#products").val();
-    
-    $.get(url + '&query=' + cokolwiek + '&key=' + apiKey, function(data){
-        if (data.cards.length == 0) {
-            $("#recipe").html('Brak przepisów. Spróbuj zmienić liczbę pojedynczą skladników na liczbę mnogą. Jeśli to nie pomoże, usuń jakiś składnik i wyszukaj ponownie');
-        } else {
-            data.cards.forEach(function(przepis) {
-                var opis = przepis.desc;
-                var tab = opis.split("\n");
-                
-                $("#recipe").html($("#recipe").html() + przepis.name + '<br>');
-                
-                for (var i = 0; i < tab.length; i++) {
-                    $("#recipe").html($("#recipe").html() + tab[i] + '<br>');
-                }
-                
-                $("#recipe").html($("#recipe").html() + '<br><hr width="80%" align="center"><br>');
-            });
-        }
-    });
+app.controller("myCtrl", function($scope, $http) {
+    $scope.sprawdz = function() {
+        var cokolwiek = $("#products").val();
+        $http.get(url + '&query=' + cokolwiek + '&key=' + apiKey).then(function(response) {
+            $scope.przepisy = response.data.cards;
+        });
+    }
 });
